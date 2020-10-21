2ced4412811ad975fa7d51d2801ed50444cbdd19
Gethryn Ghavalas <gethryn@ghavalas.com>
2020-10-21 23:30:22 +1100

    added task proportions get/set; added NA for weekend days instead of 0 (using paramater)

--- sample_data.R
+++ sample_data.R
@@ -10,9 +10,10 @@
 
 # -[TODO]-----------------------------------------------------------------------------------------------------
 # 1: better way to simulate task data so it is seasonal, has trend, possibly
-#    simulate ARIMA data or something like that?
+#    simulate ARIMA data or something like that? -- see simulate_arima.R
 # 2: use purrr to generate data functionally  √
-# 3: weekend data should be NA not 0 if not working; add na.rm's everywhere.
+# 3: weekend data should be NA not 0 if not working; add na.rm's everywhere. √
+# 4: get/set task proportions - MVP √ ; improvements TBC
 
 # -[GET CONFIG.YAML]------------------------------------------------------------------------------------------
 require(yaml); .config <- yaml::read_yaml("config.yaml")
@@ -37,6 +38,7 @@ DAYS = seq(from=START_DATE, to=END_DATE, by = "day")
 # remove volume from Sat and Sun if FALSE, work all days when TRUE
 WORK_WEEKENDS = .config$constants$WORK_WEEKENDS %||% FALSE
 WEEKEND_DAYS = .config$constants$WEEKEND_DAYS %||% c("Saturday","Sunday")
+WEEKEND_REPLACE = .config$constants$WEEKEND_REPLACE %||% NA
 
 # when simulating task counts per day, use a uniform prediction in this range 
 MIN_NUM_TASKS_PER_DAY = .config$constants$MIN_NUM_TASKS_PER_DAY %||% 0
@@ -69,8 +71,8 @@ generate_teams <- function(num_teams=NUM_TEAMS,
 
 # generate a list of task names, base volumes (for poisson process) and task lengths (secs)
 generate_tasks <- function(num_tasks, 
-                                    min_tasks = MIN_NUM_TASKS_PER_DAY, 
-                                    max_tasks = MAX_NUM_TASKS_PER_DAY) {
+                           min_tasks = MIN_NUM_TASKS_PER_DAY, 
+                           max_tasks = MAX_NUM_TASKS_PER_DAY) {
   verbs <- .config$task_names$verbs %||% c("Cleaning")
   adjectives <- .config$task_names$adjectives %||% c("Complex")
   nouns <-.config$task_names$nouns %||%  c("Documents")
@@ -85,11 +87,12 @@ generate_tasks <- function(num_tasks,
 
 # generate the data between start and end date for a single task using poisson process
 # removing weekends if we = TRUE
-generate_task_data <- function(base_value, days=DAYS, we=WORK_WEEKENDS) {
+generate_task_data <- function(base_value, days=DAYS, we=WORK_WEEKENDS, 
+                               we_replace=WEEKEND_REPLACE) {
   n = length(DAYS)
   data = rpois(n, base_value)
   out = tibble(date=DAYS, volume=data)
-  if (!we) { out$volume[weekdays(DAYS) %in% WEEKEND_DAYS] <- 0 }
+  if (!we) { out$volume[weekdays(DAYS) %in% WEEKEND_DAYS] <- we_replace }
   out
 }
 
@@ -111,16 +114,71 @@ generate_all <- function(num_teams=NUM_TEAMS, min_tasks=MIN_TASKS_PER_TEAM,
   
   # output the simulated data for each team/task combo for all DAYS
   pmap_df(ddf, 
-          function(team, task, std_time, base_vol, we=WORK_WEEKENDS, days=DAYS)
+          function(team, task, std_time, base_vol, we=WORK_WEEKENDS, days=DAYS, 
+                   we_replace=WEEKEND_REPLACE)
             tibble(team=team,
                    task=task,
                    std_time=std_time, 
-                   generate_task_data(base_vol, days, we)) %>%
+                   generate_task_data(base_vol, days, we, we_replace)) %>%
             mutate(workload = std_time * volume / 3600) %>%
             select(team, task, date, std_time, volume, workload) %>%
             as_tsibble(key=c(team,task), index=date))
   
 }
+
+#get_task_proportions <- function(dat) { }
+# generate the proportions of workload for each task/team.  If summary=F, then by month,
+# if summary=T, then average across all months.  Use summary=T to supply to set_ function.
+get_team_task_prop <- function(dat, summary=TRUE) {
+  
+  dat_team <- as_tibble(dat) %>% 
+    mutate(year_month = yearmonth(date)) %>% 
+    group_by(team, year_month) %>% 
+    summarise(team_workload = sum(workload, na.rm=TRUE)) %>% ungroup() 
+  
+  
+  dat_team_task <- as_tibble(dat) %>%
+    mutate(year_month = yearmonth(date)) %>% 
+    group_by(team, task, year_month) %>% 
+    summarise(task_workload = sum(workload, na.rm=TRUE)) %>% ungroup()
+  
+  dat_re <- as_tibble(dat) %>%
+    select(team, task, std_time) %>%
+    unique
+  
+  
+  dat_prop <- dat_team_task %>%
+    left_join(dat_team, by = c("team", "year_month")) %>%
+    left_join(dat_re, by = c("team","task")) %>%
+    mutate(prop=task_workload/team_workload)
+  
+  if (summary)
+    dat_prop %>%
+    group_by(team, task, std_time) %>% 
+    summarise(avg_task_workload = mean(task_workload, na.rm=TRUE),
+              avg_team_workload = mean(team_workload, na.rm=TRUE),
+              avg_prop=mean(prop, na.rm=TRUE))
+  else dat_prop
+  
+}
+
+# provide the summary data from get_team_task_prop, a team and the new number of hours
+# function returns expected volume and workload split across tasks
+apply_team_task_prop <- function(dat, tm, new_hours) {
+  dat %>% 
+    filter(team == tm) %>%
+    mutate(new_task_volume = round(new_hours * avg_prop * 3600 / std_time), 
+           new_task_workload = new_task_volume * std_time / 3600,
+           new_team_workload = new_hours)
+}
+
+# example call
+new_workload <- 400
+avg_team_task_props <- get_team_task_prop(dat, summary=TRUE)
+new_data <- apply_team_task_prop(avg_team_task_props, "ANGRY CHAMPIONS TEAM", new_workload)
+sum(new_data$new_task_workload) # [1] 400.0667 for 400 input 
+
+
 
 # -[CODE]-----------------------------------------------------------------------------------------------------
 
@@ -134,16 +192,16 @@ dat_nested <- dat %>%
 
 dat_weekly <- dat %>%
   group_by_key() %>%
-  index_by(week_num = ~ week(.)) %>% # weekly aggregates
+  index_by(week_num = ~ week(.x)) %>% # weekly aggregates
   summarise(volume = sum(volume, na.rm = TRUE),
-            workload = sum(workload),
+            workload = sum(workload, na.rm = TRUE),
             week_start = min(date)) 
 
 dat_monthly <- dat %>%
   group_by_key() %>%
-  index_by(year_month = ~ yearmonth(.)) %>% # monthly aggregates
+  index_by(year_month = ~ yearmonth(.x)) %>% # monthly aggregates
   summarise(volume = sum(volume, na.rm = TRUE),
-            workload = sum(workload)) 
+            workload = sum(workload, na.rm = TRUE)) 
 
 # example: hierarchical monthly forecast by team.
 hdat <- dat_monthly %>% 
